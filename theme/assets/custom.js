48290af4fdd0d572069dc22e3d1751d2636e22d0
yongri-li <yongri.liii+1@gmail.com>
2021-07-19 19:16:25 +0200

    add sticky ATC button on mobile

--- theme/assets/custom.js
+++ theme/assets/custom.js
@@ -21,3 +21,60 @@ $(document).on('click', '[data-cart-upsell-btn]', function(e){
    .catch((error) => {
    });
 });
+
+$(document).on('inview', '.product-page--cart-form', function (event, isInView) {
+  console.log('1');
+  if($(window).width() > 768) {
+    return;
+  }
+
+  console.log('2');
+
+  if($('.quanity-cart-row').is(":hidden")) {
+    return;
+  }
+
+  console.log('3');
+
+  if (isInView) {
+    $('.product-sticky-form').hide();
+    console.log('sticky header hide');
+  } else {
+    $('.product-sticky-form').show();
+    console.log('sticky header show');
+  }
+});
+
+$(document).on('click', '[data-sticky-qty-plus]', function(e){
+  e.preventDefault();
+  const $qtyInput = $('input[name="quantity"]');
+  const qty = $qtyInput.val();
+  $qtyInput.val(parseInt(qty) + 1);
+  $qtyInput.trigger('change');
+});
+
+$(document).on('click', '[data-sticky-qty-minus]', function(e){
+  e.preventDefault();
+  const $qtyInput = $('input[name="quantity"]');
+  const qty = $qtyInput.val();
+  if(qty == 1) {
+    return;
+  }
+  $qtyInput.val(parseInt(qty) - 1);
+  $qtyInput.trigger('change');
+});
+
+$(document).on('change', '[data-sticky-qty-input]', function(e){
+  e.preventDefault();
+  const $qtyInput = $('input[name="quantity"]');
+  const qty = $(this).val();
+  $qtyInput.val(parseInt(qty));
+  $qtyInput.trigger('change');
+});
+
+$(document).on('click', '[data-sticky-atc]', function(e){
+  e.preventDefault();
+  $('.add[type="submit"]').trigger('click');
+});
+
+
